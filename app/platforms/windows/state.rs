ef6a74b7a8a2b55552b064c62e2e0e3c2706cf36
Neil <cantorne.neil@gmail.com>
2026-04-24 06:01:50 +0800

    feat: add tray icon

--- app/platforms/windows/state.rs
+++ app/platforms/windows/state.rs
@@ -1,17 +1,26 @@
 use core::{
     ffi::c_void,
+    mem,
     num::NonZero,
     ptr::{self, NonNull},
 };
 
 use windows_sys::{
-    Win32::{Foundation::*, System::LibraryLoader::GetModuleHandleW, UI::WindowsAndMessaging::*},
+    Win32::{
+        Foundation::*,
+        System::LibraryLoader::GetModuleHandleW,
+        UI::{
+            Shell::{NIF_ICON, NIF_MESSAGE, NIF_TIP, NIM_ADD, NOTIFYICONDATAW, Shell_NotifyIconW},
+            WindowsAndMessaging::*,
+        },
+    },
     core::PCWSTR,
     w,
 };
 
 pub struct State {
-    manager_win: Option<NonNull<c_void>>,
+    manager_win: NonNull<c_void>,
+    is_manager_open: bool,
 }
 
 impl State {
@@ -19,18 +28,22 @@ impl State {
         let hinstance = unsafe { NonNull::new(GetModuleHandleW(ptr::null_mut())) }
             .expect("Failed to retrieve module handle");
 
+        let manager_win = unsafe { Self::new_manager_win(hinstance, config.open_manager) };
+
         Self {
-            manager_win: if config.open_manager {
-                unsafe { Some(Self::new_manager_win(hinstance)) }
-            } else {
-                None
-            },
+            manager_win,
+            is_manager_open: config.open_manager,
         }
     }
 
-    pub(super) fn enter_loop(self) {
+    pub(super) fn enter_loop(mut self) {
         unsafe {
-            let mut msg = std::mem::zeroed::<MSG>();
+            self.init_tray();
+            self.show_tray_menu();
+        };
+
+        unsafe {
+            let mut msg = mem::zeroed::<MSG>();
 
             while GetMessageW(&mut msg, ptr::null_mut(), 0, 0) > 0 {
                 TranslateMessage(&msg);
@@ -43,9 +56,7 @@ impl State {
 impl Drop for State {
     fn drop(&mut self) {
         unsafe {
-            if let Some(manager_hwnd) = self.manager_win {
-                DestroyWindow(manager_hwnd.as_ptr() as _);
-            }
+            DestroyWindow(self.manager_win.as_ptr());
         }
     }
 }
@@ -62,6 +73,7 @@ unsafe extern "system" fn window_proc(
             PostQuitMessage(0);
             0
         }
+
         _ => DefWindowProcW(hwnd, msg, wparam, lparam),
     }
 }
@@ -69,7 +81,7 @@ unsafe extern "system" fn window_proc(
 impl State {
     const SANDBOX_WIN_NAME: PCWSTR = w!("TechNoir");
 
-    unsafe fn new_manager_win(hinstance: NonNull<c_void>) -> NonNull<c_void> {
+    unsafe fn new_manager_win(hinstance: NonNull<c_void>, visible: bool) -> NonNull<c_void> {
         let wc = unsafe {
             let cursor = LoadCursorW(ptr::null_mut(), IDC_ARROW);
 
@@ -80,18 +92,24 @@ impl State {
                 lpszClassName: Self::SANDBOX_WIN_NAME,
                 hCursor: cursor,
 
-                ..std::mem::zeroed()
+                ..mem::zeroed()
             }
         };
 
         unsafe { NonZero::new(RegisterClassW(&wc)) }.expect("Failed to register window class");
 
         NonNull::new(unsafe {
+            let mut style = WS_OVERLAPPEDWINDOW;
+
+            if visible {
+                style |= WS_VISIBLE;
+            }
+
             CreateWindowExW(
                 0,
                 Self::SANDBOX_WIN_NAME,
                 Self::SANDBOX_WIN_NAME,
-                WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                style,
                 CW_USEDEFAULT,
                 CW_USEDEFAULT,
                 800,
@@ -103,5 +121,59 @@ impl State {
             )
         })
         .expect("Failed to create window")
+    }
+
+    #[allow(unsafe_op_in_unsafe_fn)]
+    unsafe fn init_tray(&self) {
+        let mut nid: NOTIFYICONDATAW = mem::zeroed();
+
+        nid.cbSize = mem::size_of::<NOTIFYICONDATAW>() as u32;
+        nid.hWnd = self.manager_win.as_ptr();
+        nid.uID = 1;
+
+        nid.uFlags = NIF_MESSAGE | NIF_TIP | NIF_ICON;
+        nid.uCallbackMessage = WM_USER + 1;
+
+        nid.hIcon = LoadIconW(ptr::null_mut(), IDI_APPLICATION);
+
+        let tip = "Open Manager\0".encode_utf16().collect::<Vec<u16>>();
+        nid.szTip[..tip.len()].copy_from_slice(&tip);
+
+        unsafe { Shell_NotifyIconW(NIM_ADD, &nid) };
+    }
+
+    #[allow(unsafe_op_in_unsafe_fn)]
+    unsafe fn show_tray_menu(&mut self) {
+        let menu = CreatePopupMenu();
+        AppendMenuW(menu, MF_STRING, 1, w!("Open Manager"));
+        AppendMenuW(menu, MF_STRING, 2, w!("Terminate"));
+        let mut pt = std::mem::zeroed();
+        GetCursorPos(&mut pt);
+        SetForegroundWindow(self.manager_win.as_ptr());
+        let cmd = TrackPopupMenu(
+            menu,
+            TPM_RETURNCMD,
+            pt.x,
+            pt.y,
+            0,
+            self.manager_win.as_ptr(),
+            std::ptr::null(),
+        );
+        match cmd {
+            1 => self.open_manager(),
+            2 => self.terminate_program(),
+            _ => {}
+        }
+        DestroyMenu(menu);
+    }
+
+    fn open_manager(&mut self) {
+        if self.is_manager_open {
+            return; // TODO
+        }
+    }
+
+    fn terminate_program(&mut self) {
+        // TODO
     }
 }

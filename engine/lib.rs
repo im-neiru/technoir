d491a4b65ce767f7a7da7d1c3fb8ddc009fa44b4
Neil <cantorne.neil@gmail.com>
2026-04-27 21:31:58 +0800

    feat: audio loopback sampler

--- engine/lib.rs
+++ engine/lib.rs
@@ -3,10 +3,12 @@ mod windows;
 
 mod config;
 mod renderer;
+mod samplers;
 mod screen_bounds;
 
 pub use config::Config;
 pub use renderer::Renderer;
+pub use samplers::AudioLoopback;
 pub use screen_bounds::ScreenBounds;
 
 #[cfg(target_family = "windows")]
---
+++ engine/samplers/mod.rs
@@ -0,0 +1,3 @@
+mod audio_loopback;
+
+pub use audio_loopback::AudioLoopback;

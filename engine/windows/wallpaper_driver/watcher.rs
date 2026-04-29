762f5b793c7833ce343bea5b1e4b0dd761ff3976
Neil <cantorne.neil@gmail.com>
2026-04-29 13:56:30 +0800

    feat: improve desktop watcher

--- engine/windows/wallpaper_driver/watcher.rs
+++ engine/windows/wallpaper_driver/watcher.rs
@@ -1,27 +1,50 @@
 use core::{
-    ptr,
+    num::NonZeroUsize,
+    ptr::{self, NonNull},
     sync::atomic::{AtomicU8, AtomicUsize, Ordering, fence},
 };
+use std::os::raw::c_void;
 
+use crossbeam_queue::SegQueue;
 use windows_sys::Win32::{
-    Foundation::HWND,
+    Foundation::{HWND, TRUE},
     UI::{
         Accessibility::{HWINEVENTHOOK, SetWinEventHook, UnhookWinEvent},
         WindowsAndMessaging::{
-            EVENT_OBJECT_LOCATIONCHANGE, EVENT_SYSTEM_FOREGROUND, WINEVENT_OUTOFCONTEXT,
-            WINEVENT_SKIPOWNPROCESS,
+            EVENT_OBJECT_LOCATIONCHANGE, EVENT_SYSTEM_FOREGROUND, IsWindowVisible,
+            WINEVENT_OUTOFCONTEXT, WINEVENT_SKIPOWNPROCESS,
         },
     },
 };
 
 static USER_COUNT: AtomicU8 = AtomicU8::new(0);
 static HOOK: AtomicUsize = AtomicUsize::new(0);
+static LAST_HWND: AtomicUsize = AtomicUsize::new(0);
+static QUEUE: SegQueue<NonZeroUsize> = SegQueue::new();
 
 pub struct WatcherGuard;
 
 pub fn start_watching() -> Option<WatcherGuard> {
+    let mut current = USER_COUNT.load(Ordering::Acquire);
+
+    while current > 0 {
+        if current == u8::MAX {
+            std::process::abort();
+        }
+
+        match USER_COUNT.compare_exchange_weak(
+            current,
+            current + 1,
+            Ordering::Relaxed,
+            Ordering::Acquire,
+        ) {
+            Ok(_) => return Some(WatcherGuard),
+            Err(actual) => current = actual,
+        }
+    }
+
     let hook = unsafe {
-        SetWinEventHook(
+        NonNull::new(SetWinEventHook(
             EVENT_SYSTEM_FOREGROUND,
             EVENT_OBJECT_LOCATIONCHANGE,
             ptr::null_mut(),
@@ -29,25 +52,28 @@ pub fn start_watching() -> Option<WatcherGuard> {
             0,
             0,
             WINEVENT_OUTOFCONTEXT | WINEVENT_SKIPOWNPROCESS,
-        )
+        ))?
     };
 
-    if hook.is_null() {
-        return None;
-    }
-
     if HOOK
-        .compare_exchange(0, hook.addr(), Ordering::Release, Ordering::Relaxed)
+        .compare_exchange(0, hook.addr().get(), Ordering::Release, Ordering::Relaxed)
         .is_err()
     {
-        unsafe { UnhookWinEvent(hook) };
+        unsafe { UnhookWinEvent(hook.as_ptr()) };
     }
 
-    if USER_COUNT.fetch_add(1, Ordering::Relaxed) == u8::MAX {
-        std::process::abort();
-    }
+    USER_COUNT.fetch_add(1, Ordering::Relaxed);
 
     Some(WatcherGuard)
+}
+
+impl WatcherGuard {
+    #[inline]
+    pub fn poll(&self) -> Option<NonNull<c_void>> {
+        let handle = QUEUE.pop()?;
+
+        Some(unsafe { NonNull::new_unchecked(handle.get() as *mut c_void) })
+    }
 }
 
 impl Drop for WatcherGuard {
@@ -76,9 +102,17 @@ unsafe extern "system" fn desktop_callback(
     _ideventthread: u32,
     _dwmseventtime: u32,
 ) {
-    if hwnd.is_null() {
+    let Some(hwnd) = NonNull::new(hwnd) else {
+        return;
+    };
+
+    if IsWindowVisible(hwnd.as_ptr()) != TRUE {
         return;
     }
 
-    // todo send
+    let addr = hwnd.addr().get();
+
+    if LAST_HWND.swap(addr, Ordering::Relaxed) != addr {
+        QUEUE.push(hwnd.addr());
+    }
 }

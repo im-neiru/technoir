f5ce1c3aa34cbc555123853c431d3f67c0c5bc79
Neil <cantorne.neil@gmail.com>
2026-05-01 17:33:03 +0800

    feat: automate entry creation with macro and only run when changed

--- app/build.rs
+++ app/build.rs
@@ -1,4 +1,5 @@
 fn main() {
+    println!("cargo:rerun-if-changed=../wallpapers/wgsl/");
     wgsl_shaders::compile_shaders("../shaders");
 
     if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
--- wallpapers/wgsl/lib.rs
+++ wallpapers/wgsl/lib.rs
@@ -4,35 +4,46 @@ pub mod compile;
 #[cfg(feature = "compile")]
 pub use compile::compile_shaders;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
-pub enum ShaderSource {
-    CosmicBubble,
+macro_rules! define_shaders {
+    ($($variant:ident => $file_name:expr),* $(,)?) => {
+        #[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+        pub enum ShaderSource {
+            $($variant),*
+        }
+
+        impl ShaderSource {
+            pub fn name(&self) -> &'static str {
+                match self {
+                    $(Self::$variant => $file_name),*
+                }
+            }
+
+            pub fn path(&self, backend: wgpu::Backend) -> String {
+                let ext = match backend {
+                    wgpu::Backend::Metal => "metal",
+                    wgpu::Backend::Vulkan => "spv",
+                    wgpu::Backend::Dx12 => "hlsl",
+                    _ => unreachable!("Unsupported backend: {:?}", backend),
+                };
+                format!("shaders/{}.{}", self.name(), ext)
+            }
+        }
+
+        #[cfg(feature = "compile")]
+        impl ShaderSource {
+            pub fn source(&self) -> &'static str {
+                match self {
+                    $(Self::$variant => include_str!(concat!($file_name, ".wgsl"))),*
+                }
+            }
+
+            pub const ALL: &[ShaderSource] = &[
+                $(ShaderSource::$variant),*
+            ];
+        }
+    };
 }
 
-impl ShaderSource {
-    fn name(&self) -> &'static str {
-        match self {
-            ShaderSource::CosmicBubble => "cosmic_bubble",
-        }
-    }
-
-    pub fn path(&self, backend: wgpu::Backend) -> &str {
-        match backend {
-            wgpu::Backend::Metal => "shaders/cosmic_bubble.metal",
-            wgpu::Backend::Vulkan => "shaders/cosmic_bubble.spv",
-            wgpu::Backend::Dx12 => "shaders/cosmic_bubble.hlsl",
-            _ => unreachable!("Unsupported backend: {backend:?}"),
-        }
-    }
-}
-
-#[cfg(feature = "compile")]
-impl ShaderSource {
-    fn source(&self) -> &'static str {
-        match self {
-            ShaderSource::CosmicBubble => include_str!("cosmic_bubble.wgsl"),
-        }
-    }
-
-    const ALL: [ShaderSource; 1] = [ShaderSource::CosmicBubble];
+define_shaders! {
+    CosmicBubble => "cosmic_bubble",
 }

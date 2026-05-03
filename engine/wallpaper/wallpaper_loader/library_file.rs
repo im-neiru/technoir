9cdae6f2337a7adbcb83bc35760cbdbe3caec571
Neil <cantorne.neil@gmail.com>
2026-05-03 13:19:07 +0800

    feat: library file toml iterator

--- engine/wallpaper/wallpaper_loader/library_file.rs
+++ engine/wallpaper/wallpaper_loader/library_file.rs
@@ -1,4 +1,4 @@
-use std::path::Path;
+use std::{path::Path, slice::Iter};
 
 use serde::{Deserialize, Serialize};
 
@@ -18,5 +18,23 @@ impl LibraryFile {
         let bytes = std::fs::read(path)?;
         let library_file: Self = toml::de::from_slice(bytes.as_slice())?;
         Ok(library_file)
+    }
+
+    #[inline]
+    pub fn iter(&self) -> LibraryFileIter {
+        LibraryFileIter(self.entries.iter())
+    }
+}
+
+pub struct LibraryFileIter<'a>(Iter<'a, WallpaperMetadata>);
+
+impl<'a> Iterator for LibraryFileIter<'a> {
+    type Item = (&'a str, &'a Path);
+
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        let entry = self.0.next()?;
+
+        Some((&entry.name, &entry.path))
     }
 }

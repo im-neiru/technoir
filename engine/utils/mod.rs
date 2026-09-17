5796bcb6a1cfe2e2d7701cdd0a69c71ba7465082
Neil <cantorne.neil@gmail.com>
2026-09-18 05:49:43 +0800

    feat: get program data path

--- engine/utils/mod.rs
+++ engine/utils/mod.rs
@@ -3,6 +3,7 @@ mod desktop_handles;
 #[cfg(target_family = "windows")]
 mod desktop_watcher;
 
+mod program_data_path;
 mod read_mapped_file;
 
 #[cfg(target_family = "windows")]
@@ -11,4 +12,5 @@ pub(crate) use desktop_handles::{DesktopHandles, get_desktop_handles};
 #[cfg(target_family = "windows")]
 pub use desktop_watcher::DesktopWatcher;
 
+pub(crate) use program_data_path::program_data_path;
 pub(crate) use read_mapped_file::ReadMappedFile;

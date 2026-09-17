61cf4fc0af29a692ba97013a8025262f9b01e32e
Neil <cantorne.neil@gmail.com>
2026-09-17 14:48:15 +0800

    feat!: overhaul to use centralize device and resource sharing

--- engine/graphics/mod.rs
+++ engine/graphics/mod.rs
@@ -1,9 +1,11 @@
 mod base;
+mod context;
 mod texture_helpers;
 mod visualizer;
 mod window_surface;
 
 pub use base::Renderer;
+pub(crate) use context::Context;
 pub use texture_helpers::{LoadTextureImageError, TextureSize};
 pub use visualizer::Visualizer;
 pub(crate) use window_surface::WindowSurface;
--- engine/graphics/window_surface.rs
+++ engine/graphics/window_surface.rs
@@ -1,22 +1,80 @@
-use wgpu::{Instance, Surface, SurfaceConfiguration, SurfaceTargetUnsafe};
+use core::num::NonZeroU16;
+use wgpu::{
+    CompositeAlphaMode, PresentMode, Surface, SurfaceColorSpace, SurfaceConfiguration,
+    SurfaceTargetUnsafe, TextureFormat, TextureUsages,
+};
 
 pub(crate) struct WindowSurface {
     pub(crate) surface: Surface<'static>,
-    pub(crate) config: Option<SurfaceConfiguration>,
+    pub(crate) config: SurfaceConfiguration,
 }
 
-impl WindowSurface {
+impl super::Context {
     #[inline]
-    pub(crate) fn new(wgpu_instance: Instance, target: impl Into<SurfaceTargetUnsafe>) -> Self {
+    pub(crate) fn create_surface(
+        &self,
+        target: impl Into<SurfaceTargetUnsafe>,
+        width: NonZeroU16,
+        height: NonZeroU16,
+    ) -> WindowSurface {
         let surface = unsafe {
-            wgpu_instance
+            self.instance
                 .create_surface_unsafe(target.into())
                 .expect("Failed to create wgpu::Surface")
         };
 
-        Self {
-            config: surface.get_configuration(),
-            surface,
-        }
+        let caps = surface.get_capabilities(&self.adapter);
+
+        let format = caps
+            .formats
+            .iter()
+            .copied()
+            .find(|&f| f == TextureFormat::Rgba8Unorm || f == TextureFormat::Bgra8Unorm)
+            .unwrap_or(caps.formats[0]);
+
+        let present_mode = if caps.present_modes.contains(&PresentMode::Mailbox) {
+            PresentMode::Mailbox
+        } else if caps.present_modes.contains(&PresentMode::Immediate) {
+            PresentMode::Immediate
+        } else {
+            PresentMode::Fifo
+        };
+
+        let alpha_mode = if caps.alpha_modes.contains(&CompositeAlphaMode::Opaque) {
+            CompositeAlphaMode::Opaque
+        } else {
+            caps.alpha_modes[0]
+        };
+
+        let config = SurfaceConfiguration {
+            usage: TextureUsages::RENDER_ATTACHMENT,
+            format,
+            width: width.get() as u32,
+            height: height.get() as u32,
+            present_mode,
+            alpha_mode,
+            view_formats: vec![],
+            desired_maximum_frame_latency: 2,
+            color_space: SurfaceColorSpace::Auto,
+        };
+
+        surface.configure(&self.device, &config);
+
+        WindowSurface { surface, config }
+    }
+}
+
+impl WindowSurface {
+    #[inline]
+    pub(crate) fn resize(
+        &mut self,
+        context: super::Context,
+        width: NonZeroU16,
+        height: NonZeroU16,
+    ) {
+        self.config.width = width.get() as u32;
+        self.config.height = height.get() as u32;
+
+        self.surface.configure(&context.device, &self.config);
     }
 }

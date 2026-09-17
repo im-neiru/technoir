1b4144aa154e90b6107e50effd0aedca9c69bd09
Neil <cantorne.neil@gmail.com>
2026-09-17 16:58:28 +0800

    fix: remove old code

--- engine/graphics/mod.rs
+++ engine/graphics/mod.rs
@@ -1,13 +1,13 @@
-mod base;
+// mod base;
 mod context;
 mod draw_context;
 mod staging_surface;
-mod texture_helpers;
-mod visualizer;
+// mod texture_helpers;
+// mod visualizer;
 mod window_surface;
 
-pub use base::Renderer;
+// pub use base::Renderer;
 pub(crate) use context::Context;
-pub use texture_helpers::TextureSize;
-pub use visualizer::Visualizer;
+// pub use texture_helpers::TextureSize;
+// pub use visualizer::Visualizer;
 pub(crate) use window_surface::WindowSurface;
